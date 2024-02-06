56cda8b84e83851d042f2666407d85e1f7440f60
thiagoifrn <thiagoifrn@gmail.com>
2024-02-06 12:00:04 -0300

    feat: Creating form component

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss',
+})
+export class FormsComponent implements OnInit {
+  results = [];
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
