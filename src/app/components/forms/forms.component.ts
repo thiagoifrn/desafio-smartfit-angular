584ae24f6148a7154aaf74d0e541a6ee44b1ab95
thiagoifrn <thiagoifrn@gmail.com>
2024-02-06 15:40:00 -0300

    feat: add form control

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss',
 })
 export class FormsComponent implements OnInit {
   results = [];
+  formGroup!: FormGroup;
+
+  constructor(private FormBuilder: FormBuilder) {}
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.formGroup = this.FormBuilder.group({
+      hour: '',
+      showClosed: false,
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.formGroup.value);
+  }
+  onClean(): void {
+    this.formGroup.reset();
   }
 }
