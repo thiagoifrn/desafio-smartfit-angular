d2b05684787addce7a28e68a3ca0ac91cc64c848
thiagoifrn <thiagoifrn@gmail.com>
2024-02-07 11:49:19 -0300

    feat: configure card and  cardList components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,33 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsComponent } from './components/forms/forms.component';
+import { CardComponent } from './components/card/card.component';
+import { BehaviorSubject } from 'rxjs';
+import { CardsListComponent } from './components/cards-list/cards-list.component';
+import { Location } from './types/locations.interface';
+import { GetUnitsService } from './services/get-units.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FormsComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FormsComponent,
+    CardComponent,
+    CardsListComponent,
+  ],
 })
 export class AppComponent {
-  title = 'desafio-smartfit';
+  showList = new BehaviorSubject(false);
+  unitList: Location[] = [];
+
+  constructor(private unitService: GetUnitsService) {}
+  onSubimit() {
+    this.unitList = this.unitService.getFilteredunits();
+    this.showList.next(true);
+  }
 }
